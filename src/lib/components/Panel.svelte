456bb1582a3f4ce1c1af9e3d50f88aabc0d6aab0
tedyeates <t3d.y34t3s@gmail.com>
2025-11-27 08:25:27 +0000

    feat: add panel

---
+++ src/lib/components/Panel.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    let { backgroundColor, width, height, ...args } = $props()
+</script>
+
+<div 
+    style:--backgroundColor={backgroundColor}
+    style:width
+    style:height
+    {...args} 
+>
+
+</div>
+
+<style lang="scss">
+    div {
+        background-color: var(--backgroundColor);
+        border-radius: 15px;
+        box-shadow: 0 4px 14px rgba(0, 0, 0, 0.6);
+        border: 2px solid color-mix(in srgb, var(--backgroundColor) 60%, white);
+    }
+</style>
---
+++ src/stories/Panel.stories.svelte
@@ -0,0 +1,18 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Panel from '$lib/components/Panel.svelte';
+
+  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+    const { Story } = defineMeta({
+        title: 'Component/Panel',
+        component: Panel,
+        tags: ['autodocs'],
+        argTypes: {
+        },
+        args: {
+        }
+    });
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+<Story name="Primary" args={{ backgroundColor: '#111111', width: "50vw", height: "90vh" }} />
